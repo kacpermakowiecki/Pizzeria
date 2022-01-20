fc08da79ce9d4c4a3745e1f8b84c63ec7bae51d7
Kacper Makowiecki <makkacperr@gmail.com>
2022-01-20 19:57:59 +0100

    debug

--- src/js/script.js
+++ src/js/script.js
@@ -369,7 +369,13 @@
       });
     }
 
-   
+    announce() {
+      const thisWidget = this;
+      const event = new CustomEvent('updated', {
+        bubbles: true
+      });
+      thisWidget.element.dispatchEvent(event);
+    }
 
 
   }
